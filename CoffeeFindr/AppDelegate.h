7ebec2432c4826103ecdb829493f58a1c9d5d486
Kellen Pierson <kellenpierson@gmail.com>
2015-08-10 21:58:58 -0700

    Initial Commit

---
+++ CoffeeFindr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoffeeFindr
+//
+//  Created by Kellen Pierson on 8/10/15.
+//  Copyright (c) 2015 Kellen Pierson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
